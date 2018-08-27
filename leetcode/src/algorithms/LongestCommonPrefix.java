7a21f298bde79df9f10d37af9948d21a2f24e51d
xuwenqiang <xuwenqiang@zbj.com>
2018-08-27 11:19:06 +0800

    check

--- leetcode/src/algorithms/LongestCommonPrefix.java
+++ leetcode/src/algorithms/LongestCommonPrefix.java
@@ -9,9 +9,33 @@ public class LongestCommonPrefix {
 		if(strs.length == 1) {
 			return strs[0];
 		} 
+		String prefix = getPrefix(strs[0], strs[1]);
+		if(strs.length == 2) {
+			return prefix;
+		}
 		
-		return "";
+		for(int i = 2;i<strs.length;i++) {
+			prefix = getPrefix(prefix, strs[i]);
+			if(prefix == "") {
+				return "";
+			}
+		}
+		
+		return prefix;
     }
+	
+	public String getPrefix(String s1,String s2) {
+		StringBuilder sb = new StringBuilder();
+		int length = s1.length()>s2.length() ? s2.length() : s1.length();
+		for(int i = 0 ;i<length; i++) {
+			if(s1.charAt(i) == s2.charAt(i)) {
+				sb.append(s1.charAt(i));
+			} else {
+				break;
+			}
+		}
+		return sb.toString();
+	}
 	
 	
 	
